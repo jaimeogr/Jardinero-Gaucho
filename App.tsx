4f86757991e0f54666deadf83f6f33bdc96718a1
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2024-09-25 09:09:51 -0300

    imported react, now problems are not displayed

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import './global.css';
 import { StyleSheet, Text, View } from 'react-native';
 

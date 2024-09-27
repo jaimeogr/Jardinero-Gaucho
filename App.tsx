5e6ec4306ac6a6b5eac0e0c239db0661893868ec
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2024-09-26 22:35:03 -0300

    gluestack is gone!

--- App.tsx
+++ App.tsx
@@ -2,20 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import './global.css';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { GluestackUIProvider } from './components/ui/gluestack-ui-provider';
-import LotesScreen from './screens/LotesScreen';
+import LotesScreen from './src/screens/LotesScreen';
 
 export default function App() {
   return (
-    <GluestackUIProvider mode="light">
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!Hola Vaquero</Text>
-        <StatusBar style="auto" />
-      </View>
-    </GluestackUIProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!Hola Vaquero</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          jsxImportSource: 'nativewind',
-        },
-      ],
-      'nativewind/babel',
-    ],
-
+    presets: ['babel-preset-expo'],
     plugins: [
       [
         'module-resolver',
         {
           root: ['./'],
-
           alias: {
-            '@': './',
+            '@': './src',
           },
         },
       ],

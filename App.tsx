1e83a6e150095f8bf5cccfc492a8e4a1987ce513
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2024-09-29 12:20:17 -0300

    removed unnecessary code

--- App.tsx
+++ App.tsx
@@ -1,31 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 import BottomTabNavigator from './src/navigation/BottonTabNavigator';
-import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
     <PaperProvider>
-      {/* <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!Hola Vaquero</Text>
-        <HomeScreen />
-        <StatusBar style="auto" />
-      </View> */}
+      <StatusBar style="auto" />
       <BottomTabNavigator />
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

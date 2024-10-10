e8b9bf9555f93a80222f7e65b49a87ff0ecfc0c1
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2024-10-10 20:51:33 -0300

    removed mobx dependencies and code for debugging that was breaking in App.tsx

--- App.tsx
+++ App.tsx
@@ -4,21 +4,12 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import BottomTabNavigator from './src/navigation/BottonTabNavigator';
-import 'mobx-react-lite/batchingForReactNative';
 
 if (__DEV__) {
   console.log('The app is running in development mode');
   import('./config/ReactotronConfig').then(() =>
     console.log('Reactotron Configured'),
   );
-
-  // Configuration for Mobx Dev Tools
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const { connectToDevTools } = require('mobx-devtools-core');
-  connectToDevTools({
-    host: 'localhost',
-    port: 8098,
-  });
 } else {
   console.log('The app is running in production mode');
 }

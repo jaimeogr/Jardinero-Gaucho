f8b764065f75fb843e195cfb4d5812c8e69b32f4
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2024-12-02 01:30:41 -0300

    created zustand stores for HomeScreen and ZoneAssignmentScreen

---
+++ src/stores/useHomeScreenStore.ts
@@ -0,0 +1,104 @@
+// useHomeScreenStore.ts
+import { create } from 'zustand';
+
+interface HomeScreenState {
+  selectedLots: Set<string>;
+  expandedZones: Set<string>;
+  expandedNeighbourhoods: Set<string>;
+
+  // Actions for selections
+  toggleLotSelection: (lotId: string, newState: boolean) => void;
+  selectLots: (lotIds: string[]) => void;
+  deselectLots: (lotIds: string[]) => void;
+  deselectAllLots: () => void;
+
+  // Actions for zone expansions / collapsing
+  toggleZoneExpansion: (zoneId: string) => void;
+  collapseAllZones: () => void;
+
+  // Actions for neighbourhoods expansions / collapsing
+  toggleNeighbourhoodExpansion: (neighbourhoodId: string) => void;
+  collapseAllNeighbourhoods: () => void;
+  expandAllNeighbourhoods: (neighbourhoodIds: string[]) => void;
+}
+
+const useHomeScreenStore = create<HomeScreenState>((set, get) => ({
+  selectedLots: new Set<string>(),
+  expandedZones: new Set<string>(),
+  expandedNeighbourhoods: new Set<string>(),
+
+  // Actions for selections
+  toggleLotSelection: (lotId, newState) => {
+    set((state) => {
+      const selectedLots = new Set(state.selectedLots);
+      if (newState) {
+        selectedLots.add(lotId);
+      } else {
+        selectedLots.delete(lotId);
+      }
+      return { selectedLots };
+    });
+  },
+  selectLots: (lotIds) => {
+    set((state) => {
+      const selectedLots = new Set(state.selectedLots);
+      lotIds.forEach((id) => selectedLots.add(id));
+      return { selectedLots };
+    });
+  },
+  deselectLots: (lotIds) => {
+    set((state) => {
+      const selectedLots = new Set(state.selectedLots);
+      lotIds.forEach((id) => selectedLots.delete(id));
+      return { selectedLots };
+    });
+  },
+  deselectAllLots: () => {
+    set(() => ({
+      selectedLots: new Set(),
+    }));
+  },
+
+  // Actions for zone expansions / collapsing
+  toggleZoneExpansion: (zoneId) => {
+    set((state) => {
+      const expandedZones = new Set(state.expandedZones);
+      if (expandedZones.has(zoneId)) {
+        expandedZones.delete(zoneId);
+      } else {
+        expandedZones.add(zoneId);
+      }
+      return { expandedZones };
+    });
+  },
+  collapseAllZones: () => {
+    set(() => ({
+      expandedZones: new Set(),
+    }));
+  },
+
+  // Actions for neighbourhoods expansions / collapsing
+  toggleNeighbourhoodExpansion: (neighbourhoodId) => {
+    set((state) => {
+      const expandedNeighbourhoods = new Set(state.expandedNeighbourhoods);
+      if (expandedNeighbourhoods.has(neighbourhoodId)) {
+        expandedNeighbourhoods.delete(neighbourhoodId);
+      } else {
+        expandedNeighbourhoods.add(neighbourhoodId);
+      }
+      return { expandedNeighbourhoods };
+    });
+  },
+  collapseAllNeighbourhoods: () => {
+    set(() => ({
+      expandedNeighbourhoods: new Set(),
+    }));
+  },
+  expandAllNeighbourhoods: (neighbourhoodIds) => {
+    set(() => ({
+      expandedNeighbourhoods: new Set(neighbourhoodIds),
+    }));
+  },
+}));
+
+export default useHomeScreenStore;

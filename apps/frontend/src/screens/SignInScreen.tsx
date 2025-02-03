83d5ca337f5d640555e90e9af310bcecfca40930
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2025-02-02 21:26:59 -0300

    creation of users is working! clerk is working fantastic and in an easy way it seems

--- apps/frontend/src/screens/SignInScreen.tsx
+++ apps/frontend/src/screens/SignInScreen.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import { useSignIn } from '@clerk/clerk-expo';
+import React, { useState, useCallback } from 'react';
+import { Button, View, Text, StyleSheet, TextInput } from 'react-native';
 
 const SignInScreen = ({ navigation }) => {
+  const { signIn, setActive, isLoaded } = useSignIn();
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Handle the submission of the sign-in form
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) return;
+
+    // Start the sign-in process using the email and password provided
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: emailAddress,
+        password,
+      });
+
+      // If sign-in process is complete, set the created session as active
+      // and redirect the user
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId });
+        console.log('Sign in complete, now I should redirect the user');
+      } else {
+        // If the status isn't complete, check why. User might need to
+        // complete further steps.
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, emailAddress, password]);
+
   return (
     <View style={styles.container}>
-      <Text>Sign In Screen</Text>
-      <Button title="Don't have an account? Sign Up" onPress={() => navigation.navigate('SignUp')} />
+      <TextInput
+        autoCapitalize="none"
+        value={emailAddress}
+        placeholder="Enter email"
+        onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+      />
+      <TextInput
+        value={password}
+        placeholder="Enter password"
+        secureTextEntry={true}
+        onChangeText={(password) => setPassword(password)}
+      />
+      <Button title="Sign in" onPress={onSignInPress} />
+      <View>
+        <Text>Dont have an account?</Text>
+        <Button title="Don't have an account? Sign Up" onPress={() => navigation.navigate('SignUp')} />
+      </View>
     </View>
   );
 };

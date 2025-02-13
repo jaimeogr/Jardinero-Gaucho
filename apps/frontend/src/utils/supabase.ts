f3c6a2364fa0486a669f7856a87b2d58818b75aa
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2025-02-13 01:30:10 -0300

    included code for future supabase implementation

--- apps/frontend/src/utils/supabase.ts
+++ apps/frontend/src/utils/supabase.ts
@@ -1,3 +1,8 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.EXPO_PUBLIC_SUPABASE_URL, process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.EXPO_PUBLIC_SUPABASE_URL, process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+  },
+});
